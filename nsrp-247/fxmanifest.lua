90c2ed84cfe7a189676c61f00085e5dc67ef76af
Toasty <72546188+ToastyOfficial@users.noreply.github.com>
2020-10-08 04:32:31 -0400

    Add files via upload

---
+++ nsrp-247/fxmanifest.lua
@@ -0,0 +1,14 @@
+description 'Night Shift Studios Vinewood 247'
+name 'NightShiftRP nsrp-247'
+author 'NightShiftRP [Toasty] '
+
+files {
+    "interiorproxies.meta"
+}
+
+data_file 'INTERIOR_PROXY_ORDER_FILE' 'interiorproxies.meta'
+
+this_is_a_map 'yes'
+
+fx_version 'adamant'
+games {'gta5'}
